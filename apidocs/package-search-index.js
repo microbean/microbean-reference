4d0da53e6a62f77e9cab38688cb51f5a230ad46f
microbean <ci@microbean.org>
2026-02-21 07:14:55 +0000

    Site checkin for project microBean™ Reference

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.microbean.reference","l":"org.microbean.reference"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"org.microbean.reference","l":"org.microbean.reference"}];updateSearchResults();
